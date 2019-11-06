9657ed369d757d668ed6dcbf71cccd189f622f2a
Nate <nhays@ycp.edu>
2019-11-06 10:06:04 -0500

    Firebase sign in complete

--- app/src/main/java/com/picto/ycpcs/myapplication/CameraActivity.java
+++ app/src/main/java/com/picto/ycpcs/myapplication/CameraActivity.java
@@ -41,6 +41,7 @@ public class CameraActivity extends AppCompatActivity {
         Toast.makeText(CameraActivity.this, "Picto app started", Toast.LENGTH_LONG).show();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        /*
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,10 +49,11 @@ public class CameraActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+        */
 
         // get global data reference
-        applicationState = ((ApplicationState)getApplicationContext());
-        applicationState.setParentActivity(this);
+        //applicationState = ((ApplicationState)getApplicationContext());
+        //applicationState.setParentActivity(this);
 
         loadSettings(); // load user settings
     }
--- app/src/main/java/com/picto/ycpcs/myapplication/LoginActivity.java
+++ app/src/main/java/com/picto/ycpcs/myapplication/LoginActivity.java
@@ -71,7 +71,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithCustomToken:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "User Authenticated",
+                            Toast.makeText(LoginActivity.this, "User created!",
                                     Toast.LENGTH_SHORT).show();
                         } else {
                             // If sign in fails, display a message to the user.
