c8eaee07cf7c90a8936e61b568fd54f697447cdb
nhays1 <nhays@ycp.edu>
2019-11-11 19:57:19 -0500

    Revert "Merge branch 'firebase'"
    This reverts commit 7343e6c3a0116bf9965537567156455ebedbd98a, reversing
    changes made to 67cd593d27c983d088662fd432de0af58c2e227d.

--- app/src/main/java/com/picto/ycpcs/myapplication/LoginActivity.java
+++ app/src/main/java/com/picto/ycpcs/myapplication/LoginActivity.java
@@ -2,127 +2,23 @@ package com.picto.ycpcs.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+public class LoginActivity extends AppCompatActivity {
 
-<<<<<<< HEAD
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-
-    EditText emailText, passwordText;
-    String email, password;
-    private FirebaseAuth mAuth;
-    private static final String TAG = "CustomAuthActivity";
-=======
     EditText username, password;
     ApplicationState applicationState = null;
 
->>>>>>> master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-<<<<<<< HEAD
-
-        // Views
-        emailText = findViewById(R.id.email);
-        passwordText = findViewById(R.id.password);
-
-        // Buttons
-        findViewById(R.id.login).setOnClickListener(this);
-        findViewById(R.id.createAccount).setOnClickListener(this);
-
-        email = emailText.toString();
-        password = passwordText.toString();
-        mAuth = FirebaseAuth.getInstance();
-    }
-
-    @Override
-    public void onClick(View view){
-        int i = view.getId();
-        if (i == R.id.createAccount) {
-            createAccount(emailText.getText().toString(), passwordText.getText().toString());
-        } else if (i == R.id.login) {
-            signIn(emailText.getText().toString(), passwordText.getText().toString());
-        }
-    }
-
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-    }
-
-    private void createAccount(String email, String password){
-        //TODO Validate credentials are correct
-        Log.d(TAG, "createAccount:" + email);
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCustomToken:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "User created!",
-                                    Toast.LENGTH_SHORT).show();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
-    private void signIn(String email, String password) {
-        //TODO Validate credentials are correct
-        Log.d(TAG, "signIn:" + email);
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCustomToken:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                            Toast.makeText(LoginActivity.this, "User Authenticated",
-                                    Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent(LoginActivity.this, CameraActivity.class);
-                            startActivity(intent);
-                            updateUI(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            Toast.makeText(LoginActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-
-                        // ...
-                    }
-                });
-    }
-
-    private void updateUI(FirebaseUser user) {
-        //TODO change UI when called
-=======
         applicationState = ((ApplicationState)getApplicationContext());
 
         username = (EditText)findViewById(R.id.username);
@@ -132,7 +28,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         password.setText(applicationState.password());
 
 
->>>>>>> master
     }
 
     @Override
@@ -156,10 +51,9 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         return super.onOptionsItemSelected(item);
     }
 
-/*
-    public void logInOld(View view) {
-        if(isValidInput(emailText.getText().toString(), passwordText.getText().toString())){
-            if(emailText.getText().toString().equals("admin") && passwordText.getText().toString().equals("admin")){
+    public void login(View view) {
+        if(isValidInput(username.getText().toString(), password.getText().toString())){
+            if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
                 //correct password
                 Toast toast = Toast.makeText(LoginActivity.this, "Login Successful", Toast.LENGTH_LONG);
                 toast.show();
@@ -178,7 +72,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             toast.show();
         }
     }
-*/
 
     public static boolean isValidInput(String user, String pword){
 
